a7f08883b97ffc13507009f8b4de62c0584b8365
Sandalu De Silva <86417299+Sandalu2001@users.noreply.github.com>
2025-02-18 00:40:24 +0530

    Make service directory as package

--- backend/service/index.py
+++ backend/service/index.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from controllers.image import image_controller
+
+import os, sys
+sys.path.insert(0, os.path.abspath('./detectron2'))
+
+from controllers.image_controller import image_controller
 
 app = Flask(__name__)
-app.register_blueprint(image_controller)  # Register the controller
+app.register_blueprint(image_controller) 
 
 if __name__ == "__main__":
     app.run(debug=True)
