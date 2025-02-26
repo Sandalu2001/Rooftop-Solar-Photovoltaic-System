184a8cff0af347b2a3b72c998a7471c811995da6
Sandalu De Silva <86417299+Sandalu2001@users.noreply.github.com>
2025-02-26 12:57:51 +0530

    Refactor Views component to remove unused imports and update View object

--- webapp/src/pages/Views.tsx
+++ webapp/src/pages/Views.tsx
@@ -1,14 +1,7 @@
 import React, { lazy } from "react";
-const cashierPanel = lazy(() => import("./cashier-dashboard/CashierPanel"));
-const inventoryPanel = lazy(() => import("./product-inventory/InventoryPanel"));
-const analyticsPanel = lazy(
-  () => import("./analytics-dashboard/InventoryPanel")
-);
+
 const userAdminPanel = lazy(() => import("./tool/Tool"));
 
 export const View = {
-  cashierPanel,
-  inventoryPanel,
-  analyticsPanel,
   userAdminPanel,
 };
