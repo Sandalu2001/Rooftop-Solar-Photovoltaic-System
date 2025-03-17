5a355607e4e42b565059eda048636dc8ab4a977e
Sandalu De Silva <86417299+Sandalu2001@users.noreply.github.com>
2025-03-17 23:46:23 +0530

    Refactor theme.ts to remove unused imports and styles

--- webapp/src/theme.ts
+++ webapp/src/theme.ts
@@ -1,6 +1,5 @@
 import { Theme, alpha, createTheme } from "@mui/material/styles";
 import { green } from "@mui/material/colors";
-import type {} from "@mui/x-data-grid-pro/themeAugmentation";
 import { borderRadius, lineHeight } from "@mui/system";
 // import "./App.css";
 
@@ -49,16 +48,6 @@ export const theme = createTheme({
     },
   },
   components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          background: "#E55800",
-          "&:hover": {
-            backgroundColor: "#db5807",
-          },
-        },
-      },
-    },
     MuiTextField: {
       styleOverrides: {
         root: {
@@ -80,83 +69,6 @@ export const theme = createTheme({
           "& .MuiDialogTitle-root": {
             fontWeight: "bold",
           },
-        },
-      },
-    },
-
-    MuiDataGrid: {
-      styleOverrides: {
-        root: {
-          fontSize: 13,
-          pb: 0,
-          width: "100%",
-          borderLeft: "none",
-          borderRight: "none",
-          borderBottom: "none",
-          color: alpha("#000", 0.55),
-          borderRadius: 0,
-          "& .MuiDataGrid-cell:focus": {
-            outline: "none",
-          },
-          "& .MuiDataGrid-cell:focus-within": {
-            outline: "none",
-          },
-          ".MuiDataGrid-columnSeparator": {
-            display: "none",
-          },
-          "& .MuiDataGrid-columnHeader:focus": {
-            outline: "none",
-          },
-          "& .MuiDataGrid-columnHeader-within": {
-            outline: "none",
-          },
-          "& .MuiDataGrid-columnHeader:focus-within": {
-            outline: "none",
-          },
-          ".MuiDataGrid-row:focus": {
-            borderRadius: 0,
-          },
-          ".MuiDataGrid-row:focus-within": {
-            borderRadius: 0,
-          },
-          "& .MuiDataGrid-columnHeaderCheckbox .MuiDataGrid-columnHeaderTitleContainer":
-            {
-              display: "none",
-            },
-          ".MuiDataGrid-columnHeaderTitle": {
-            fontSize: 13,
-            fontWeight: "bold",
-          },
-          "& .MuiDataGrid-columnHeader": {
-            background: "none",
-            fontWeight: "bold",
-            borderRadius: 0,
-          },
-          "& .MuiDataGrid-columnHeaderRow": {
-            background: "none",
-            fontWeight: "bold",
-            borderRadius: 0,
-          },
-          "& .MuiDataGrid-columnHeader--withLeftBorder": {
-            borderRadius: 10,
-          },
-          "& .MuiChartsContainer-root": {
-            padding: 0, // Reduce chart container padding
-            margin: 0, // Reduce chart container margin
-          },
-          "& .MuiChartsAxis-root": {
-            padding: 0, // Reduce axis padding
-          },
-          "& .MuiChartsAxis-tickContainer": {
-            margin: 0, // Reduce tick container margin
-          },
-        },
-        columnHeader: {
-          mb: 2,
-          minHeight: 60,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
         },
       },
     },
