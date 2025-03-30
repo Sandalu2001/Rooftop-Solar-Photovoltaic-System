f51ed07ef323038708f3719974a582c869f650fd
Sandalu De Silva <86417299+Sandalu2001@users.noreply.github.com>
2025-03-30 23:28:34 +0530

    Refactor Flask app to enable CORS for image uploads

--- backend/service/index.py
+++ backend/service/index.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 import os, sys
 sys.path.insert(0, os.path.abspath('./detectron2'))
@@ -6,6 +7,7 @@ sys.path.insert(0, os.path.abspath('./detectron2'))
 from controllers.image_controller import image_controller
 
 app = Flask(__name__)
+CORS(app) 
 app.register_blueprint(image_controller) 
 
 if __name__ == "__main__":
